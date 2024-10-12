21d49a90dc9b56d4d356f8c00aef6654eccf41a9
Amirhosein Imani <imaniamirhosein2001@gmail.com>
2024-10-12 14:37:03 +0330

    finish auto text effect animation--commit

--- auto-text-effect-animation/script.js
+++ auto-text-effect-animation/script.js
@@ -1,3 +1,30 @@
 const containerElement = document.querySelector('.container')
 
-containerElement.innerHTML = `<h1>I am a programmer</h1>`
+const items = ["Amirhosein Imani" , "Web Developer" , "Programmer" , "Freelancer"]
+
+let itemIndex = 0 
+let characterIndex = 0 
+
+updateText();
+
+function updateText(){
+    characterIndex++
+    
+    if (itemIndex === 0) {
+        containerElement.innerHTML = `<h1>I am ${items[itemIndex].slice(0,characterIndex)}</h1>`
+    }
+    else{
+        containerElement.innerHTML = `<h1>I am a ${items[itemIndex].slice(0,characterIndex)}</h1>`
+    }
+
+    if (characterIndex === items[itemIndex].length) {
+        itemIndex++
+        characterIndex = 0
+    }
+    
+    if (itemIndex === items.length) {
+        itemIndex = 0
+    }
+
+    setTimeout(updateText , 200);
+}
