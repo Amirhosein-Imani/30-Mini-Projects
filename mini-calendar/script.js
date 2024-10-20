5534cb39c7dd8cd109a2be77a0d5ab5101f10d48
Amirhosein Imani <imaniamirhosein2001@gmail.com>
2024-10-20 15:01:52 +0330

    finish mini calendar--commit

---
+++ mini-calendar/script.js
@@ -0,0 +1,12 @@
+const monthNameElement = document.getElementById('month-name')
+const dayNameElement = document.getElementById('day-name')
+const dayNumberElement = document.getElementById('day-number')
+const yearElement = document.getElementById('year')
+
+const date = new Date();
+
+const month = date.getMonth()
+monthNameElement.innerText = date.toLocaleString('en' , {month:'long'})
+dayNameElement.innerText = date.toLocaleString('en' , {weekday:'long'})
+dayNumberElement.innerText = date.getDate()
+yearElement.innerText = date.getFullYear()
